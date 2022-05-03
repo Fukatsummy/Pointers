d1c75ad9129137a6b37fb4bfa660150e09410969
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-05-03 19:24:41 +0300

    Encoding

--- DinamicMemory2/main.cpp
+++ DinamicMemory2/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
 int* push_back(int arr[], int n, int value);
+//int* push_front(int arr[], int n, int value);
 
 void main()
 {
@@ -20,19 +21,36 @@ void main()
 	arr = push_back(arr, n, value);
 	Print(arr, n+1);
 	delete[] arr;
+	cout << "Введите добавляемое в начало массива значение: "; cin >> value;
+	/*arr = push_front(arr, n, value);
+	Print(arr, n + 1);
+	delete[] arr;*/
+	//arr = push_front(arr, n, value);
+	int*arr2 = new int[n + 1];
+	arr2[n] = value;
+	for (int i = 1; i < n+1; i++)
+	{
+		arr2[i+1] = arr[i];
+	}
+	//n++;
+	delete[] arr;
+	arr = arr2;
+	Print(arr, n+1);
+	//arr2 = nullptr;
+
 	//Моя верия решения
 	/*int*arr2 = new int[n+1];
-	for (int i = 0; i < n; i++)
+	  for (int i = 0; i < n; i++)
 	{
-	arr2[i] = arr[i];
+	  arr2[i] = arr[i];
 	}
-	delete*arr;
-	arr = arr2;
-	arr2=nullptr;
-	int value;
-	cout << "Введите добавляемое значение: "; cin >> value;
-	arr[n]=value;
-	n++;
+	  delete[]arr;
+	  arr = arr2;
+	  arr2=nullptr;
+	  int value;
+	  cout << "Введите добавляемое значение: "; cin >> value;
+	  arr[n]=value;
+	  n++;
 	*/
 }
 void FillRand(int arr[], const int n)
@@ -68,3 +86,13 @@ int* push_back(int arr[], int n, int value)
 	//Mission complete -элемент добавлен. 
 	return arr;
 }
+//int* push_front(int arr[], int n, int value)
+//{
+//	int*buffer2 = new int[n + 1];
+//	for (int i = 1; i < n; i++)buffer2[i] = arr[i-1];
+//	delete[]arr;
+//	arr = buffer2;
+//	//arr[n] = value;
+//	n++;
+//	return arr;
+//}
