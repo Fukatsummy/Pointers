c240e37ab231be85367074b4558486cb15193320
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-05-03 12:20:58 +0300

    todo

--- DinamicMemory/main.cpp
+++ DinamicMemory/main.cpp
@@ -2,20 +2,98 @@
 using namespace std;
 
 #define tab "\t"
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	int n;
-	cout << "¬ведите размер массива: "; cin >> n;
-	int* arr = new int[n];
+	int n=10;
+	/*int a = 0;
+	int b = 0;
+	int*even=new int[a];
+	int*odd=new int[b];*/
+	//cout << "¬ведите размер массива: "; cin >> n;
+	int*arr = new int[n];
+	cout << typeid(arr).name() << endl;//
+	FillRand(arr, n);
+	Print(arr, n);
+	int value;
+	cout << "¬ведите добавл€емое значение: "; cin >> value;
+	arr[n] = value;
+	n++;
+	Print(arr, n);
+	/*const int SIZE = 5;
+	int brr[SIZE];
+	cout << typeid(brr).name() << endl;
+	FillRand(brr, SIZE);
+	Print(brr, SIZE);
+	cout << sizeof(int) << endl;*/
+	delete[] arr;
+	
+	//delete[] even;
+	//delete[] odd;
+	//for (int i = 0; i < n; i++)
+	//{
+	//	*(arr+i)=rand()% 100;
+	//}
+	//for (int i = 0; i < n; i++)
+	//{
+	//	cout << arr[i] << tab;
+	//	//cout << arr + i << endl;
+	//}
+	//cout << endl;
+	/*for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)
+		{
+			even[a] = arr[i];
+			even++;
+		}
+		else
+		{
+			odd[b] = arr[i];
+			odd++;
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < a; i++)
+		cout << even[i] << endl;
+	for (int i = 0; i < b; i++)
+		cout << odd[i] << endl;*/
+	/*for (int i = 0; i < n; i++)	
+	{
+		if (arr[i] % 2 == 0)
+			even[n] = arr[i];
+		
+	}*/
+	//for (int i = 0; i <= even; i++)
+	//{
+	//	int n = arr[i];
+	//	if (n%2 == 0)
+	//	{
+	//		for (int j = i - 1; j >= 0 && arr[j]>n ; --j)
+	//		{
+	//			arr[j + 1] = arr[j];
+	//		}
+	//		//arr[ j + 1] = n;
+	//	}
+	//}
+}
+
+void FillRand(int arr[], const int n)
+{
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 100;
+		*(arr+i)=rand() % 100;
 	}
+}
+void Print(int arr[], const int n)
+{
+	cout << typeid(arr).name() << endl;
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << tab;
 	}
-	cout << endl;
+    cout << endl;
 }
---
+++ DinamicMemory2/main.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+int* push_back(int arr[], int n, int value);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер массива: "; cin >> n;
+	int*arr = new int[n];
+	cout << typeid(arr).name() << endl;
+	FillRand(arr, n);
+	Print(arr, n);
+	int value;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr[n] = value;
+	n++; //увеличивает количество элементов в массиве
+	Print(arr, n);
+	delete[]arr;
+	arr = push_back(arr, n, value);
+	Print(arr, n+1);
+	delete[] arr;
+	//int*arr2 = new int[n];
+ 
+	/*for (int i = 0; i < n; i++)
+	arr2[i] = arr[i];
+	arr = arr2;
+	arr[n] = n + 1;*/
+	/*int m = (n + value);
+	int*arr2 = new int[m];
+	cout << typeid(arr).name() << endl;*/
+	/*arr = arr2;
+	delete[] arr;
+	delete[]arr2;*/
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	cout << typeid(arr).name() << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+int* push_back(int arr[], int n, int value)
+{
+	//Создаем буферный массив нужного размера в данном случае на 1 элемент больше
+	int*buffer = new int[n + 1];
+	//Копируем все данные из исходного массива в буферный
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	//После того как данные скопированы в новый массив, старый массив уже не нужен
+	delete[]arr;
+	//Подменяем адрес старого массива адресом нового массива.
+	arr = buffer;
+	//Только после всего этого в новый массив можно добавить значение.
+	arr[n] = value;
+	//После добавления элемента в массив, количество его элементов увеличится на 1.
+	n++;
+	//Mission complete -элемент добавлен. 
+	return arr;
+}
